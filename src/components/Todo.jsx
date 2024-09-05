e2d4ef9ca8a5f0e3eb26df0f0d35df8265401084
Jeslin-0107 <jeslinrajinikanth35@gmail.com>
2024-09-05 13:30:16 +0530

    Initial commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,80 @@
+import  { useEffect, useRef, useState } from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
+
+const Todo = () => {
+
+    const [todoList, setTodoList] = useState(localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : []);
+
+    const inputRef = useRef();
+
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+        const newTodo = {
+            id : Date.now(),
+            text : inputText,
+            isComplete : false,
+        }
+        setTodoList((prev)=> [...prev, newTodo]);
+        inputRef.current.value = '';
+    }
+
+    const deleteTodo = (id) => {
+       setTodoList((prvTodos)=> {
+           return prvTodos.filter((todo)=> todo.id !== id)
+       })
+    }
+
+    const toggle = (id) => {
+        setTodoList((prevTodos)=> {
+            return prevTodos.map((todo)=> {
+                if(todo.id === id){
+                    return {...todo, isComplete : !todo.isComplete}
+                }
+                return todo
+            })
+        })
+    }
+
+    useEffect(()=> {
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+    },[todoList])
+
+
+
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+
+{/* //tittle */}
+      
+    <div className='flex items-center mt-7 gap-2'>
+        <img className='w-8' src={todo_icon} alt=''/>  
+        <h4 className='text-2xl font-bold'>To-Do List</h4>
+    </div>
+
+{/* //input */}
+
+    <div className='flex items-center my-7 bg-neutral-200  rounded-full'>
+        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add Your Task'/>
+        <button onClick={add} className='border-none rounded-full bg-yellow-400 w-32 h-14 text-color-white text-large font-medium cursor-pointer '>ADD </button>
+    </div>
+
+{/* //Todo List */}
+
+    <div>
+
+        {todoList.map((item, index)=>{
+            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
+        })}
+        {/* because of multiple components we will create new component */}
+        {/* <TodoItems text="Learn React"/>
+        <TodoItems text="Practice React"/> */}
+    </div>
+
+
+
+    </div>
+  )
+}
+
+export default Todo
